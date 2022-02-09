60a38c6aba7c31656bb97ac1214eb7a84f9c7f6f
Rohan K. Thakur <rohan.k.thakur@gmail.com>
2022-02-09 15:00:49 -0800

    finished taking in and printing out config values

--- src/Network.cpp
+++ src/Network.cpp
@@ -6,31 +6,49 @@
 * based upon input configuration parameters
 */
 
-// TODO: include list of functions in the module in the header comment
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-boolean training;           // training represents whether the network is training or not
-// TODO: boolean that represents whether we're loading in random weights for training or set weights
-// TODO: for training
-int N, A, B, F;             // N is the number of connectivity layers in the network, A is the
-                            // number of activation nodes in the input activation layer, B is the
-                            // number of nodes in the hidden layer, and F is the number of output
-                            // nodes
-vector<double> weights[][]; // the weights array where the first index represents the connectivity
-                            // layer, and the second and third indices represent the node TODO FINISH THIS COMMENT
+bool training;                            // training represents whether the network is in training
+                                          // mode
+int A, B, F;                              // A is the number of activation nodes in the input
+                                          // activation layer, B is the number of nodes in the
+                                          // hidden layer, and F is the number of output nodes
+int numLayers;                            // the number of connectivity layers in the network
+vector<vector<vector<double> > > weights; // weights is the weights array, where the first index
+                                          // represents the connectivity layer of an edge, and the
+                                          // second and third indices represent the node from which
+                                          // the edge originates and the node to which the edge
+                                          // goes, respectively. The value contained in a given
+                                          // position in weights is the weight of the corresponding
+                                          // edge
+vector<vector<double> > T;                // T is the truth table, where the first index represents
+                                          // the test case index, and the second index represents
+                                          // the index of the node. Note that the values contained
+                                          // in all but the last entry of any given test case are
+                                          // the input activation layer values, and the last entry
+                                          // in any given test case is the expected output value
+double lambda;                            // the learning factor value
+double errorThreshold;                    // the error threshold for terminating training
+int maxIterations;                        // the maximum number of iterations after which training
+                                          // will terminate
+bool useRandWeights;                      // useRandWeights represents whether the network
+                                          // should be using random weights or set weights for
+                                          // training
+double minRandVal, maxRandVal;            // the minimum and maximum random values to use for random
+                                          // weight values
 
 /**
-* Prompts the user for the name of the input configuration parameter file to use and then sets that
-* file as the standard input stream
+* Prompts the user with the input message string for the name of the file from which to read and
+* then sets that file as the standard input stream
 */
-void setupFileInput()
+void setupFileInputWithMessage(string message)
 {
    string filename;
-   cout << "What is the name of your input parameter file?\n";
+   cout << message << "\n";
    cin >> filename;
+   cout << "\n"; // add a new line after the user's input to make the program output easier to read
    freopen(filename.c_str(), "r", stdin);
 } // void setupFileInput(string filename)
 
@@ -38,25 +56,50 @@ void setupFileInput()
 * Takes in the input configuration parameters and sets their corresponding global variable values
 * accordingly
 */
-// takes in input parameters
 void config()
 {
-
-}
-
-// prints out input parameters
+   setupFileInputWithMessage("What is the name of your input parameter file?");
+   cin >> numLayers >> A >> B >> F;
+   cin >> training;
+   if (training) cin >> lambda >> errorThreshold >> maxIterations >> useRandWeights;
+   if (useRandWeights) cin >> minRandVal >> maxRandVal;
+} // void config()
 
 /**
-* The main method which either trains or executes the network, depending upon the input parameters
+* Prints out the configuration parameters used
+*/
+void printOutConfigVals()
+{
+   cout << "numLayers: " << numLayers << ", A: " << A << ", B: " << B << ", F: " << F << "\n";
+   cout << "training: " << training << "\n";
+
+   if (training)
+   {
+      cout << "lambda: " << lambda << ", errorThreshold: " << errorThreshold << ", maxIterations: ";
+      cout << maxIterations << ", useRandWeights: " << useRandWeights << "\n";
+   } // if (training)
+
+   if (useRandWeights)
+   {
+      cout << "minRandVal: " << minRandVal << ", maxRandVal: " << maxRandVal << "\n";
+   } // if (useRandWeights)
+} // void printOutConfigVals()
+
+/**
+* The main method which either trains or executes the network, depending upon the configuration
+* parameters
 */
 int main()
 {
-   setupFileInput();
+   config();
    // TODO: make everything their own functions
-   // TODO: write down the expected outline of the input parameter file in the README file
-   cin >> N >> A >> B >> F;
+   // TODO: write down the expected structure of the input parameter file in the README file
+   // i.e. numLayers, A, B, F, training, lambda, errorThreshold, maxIterations, useRandWeights,
+   // minRandVal, maxRandVal
 
-   cout << "N: " << N << ", A: " << A << ", B: " << B << ", F: " << F << "\n";
+   // TODO: make the iterator variables k, j, and i as set in the design doc based on which layer
+   // TODO: I'm iterating through
+   printOutConfigVals();
 
    // TODO: make the weights file a separate file from the config file
 
