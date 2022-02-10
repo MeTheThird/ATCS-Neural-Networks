e5192d6d5f8754abcd9e01bade2eb90192333cd8
Rohan K. Thakur <rohan.k.thakur@gmail.com>
2022-02-09 18:06:22 -0800

    finished the allocate memory function

--- src/Network.cpp
+++ src/Network.cpp
@@ -23,12 +23,14 @@ vector<vector<vector<double> > > weights; // weights is the weights array, where
                                           // goes, respectively. The value contained in a given
                                           // position in weights is the weight of the corresponding
                                           // edge
-vector<vector<double> > T;                // T is the truth table, where the first index represents
+vector<vector<double> > truth;            // T is the truth table, where the first index represents
                                           // the test case index, and the second index represents
                                           // the index of the node. Note that the values contained
                                           // in all but the last entry of any given test case are
                                           // the input activation layer values, and the last entry
                                           // in any given test case is the expected output value
+vector<double> activations;               // the input activation layer values to use to run the
+                                          // network if the network is in running mode
 double lambda;                            // the learning factor value
 double errorThreshold;                    // the error threshold for terminating training
 int maxIterations;                        // the maximum number of iterations after which training
@@ -86,22 +88,57 @@ void printOutConfigVals()
 } // void printOutConfigVals()
 
 /**
+* Allocates the necessary memory for the global variables based upon the configuration parameters
+*/
+void allocateMemory()
+{
+   weights.resize(numLayers);
+   for (int n = 0; n < numLayers; n++)
+   {
+      if (n)
+      {
+         weights[n].resize(B); // OR weights[n].resize(weights[n-1][0].size());
+         for (int j = 0; j < B; j++)
+         {
+            weights[n][j].resize(F);
+         } // for (int j = 0; j < B; j++)
+      } // if (n)
+      else
+      {
+         weights[n].resize(A);
+         for (int k = 0; k < A; k++)
+         {
+            weights[n][k].resize(B);
+         } // for (int k = 0; k < A; k++)
+      } // else TODO: FIX THIS COMMENT
+   } // for (int n = 0; n < numLayers; n++)
+} // void allocateMemory()
+
+/**
 * The main method which either trains or executes the network, depending upon the configuration
 * parameters
 */
 int main()
 {
    config();
+   printOutConfigVals();
+   allocateMemory();
    // TODO: make everything their own functions
    // TODO: write down the expected structure of the input parameter file in the README file
    // i.e. numLayers, A, B, F, training, lambda, errorThreshold, maxIterations, useRandWeights,
    // minRandVal, maxRandVal
 
    // TODO: make the iterator variables k, j, and i as set in the design doc based on which layer
-   // TODO: I'm iterating through
-   printOutConfigVals();
+   // TODO: I'm iterating through i.e. k for activation layer, j for hidden layer, i for output
 
    // TODO: make the weights file a separate file from the config file
+
+   // TODO: how to do a closing loop comment for the else portion of if-else statements? This is
+   // relevant to the allocateMemory() method (check the line with OR in a comment and delete the
+   // comment after resolving this question)
+
+   // TODO: do offsets by 1 or calling [0] count as magic numbers? Relevant to the allocateMemory()
+   // method
 
 } // int main()
 
